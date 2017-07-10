af8e5f2f975562ada8d4626d498718d089570e76
Franklin Cerasoli <franklincerasoli@Franklins-MBP.home>
2017-07-10 18:53:37 -0500

    Added HashLife

--- ConwayLife.py
+++ ConwayLife.py
@@ -53,20 +53,14 @@ spaceshipPattern[3,1:5] = spaceshipPattern[1:3,4] = True
 
 class Life():
 
-  def __init__ ( self, wHeight=400, wWidth=400, bWidth=25, nX=0, nY=0 ):
+  def __init__ ( self, wHeight=400, wWidth=400, bWidth=15, pow2=4 ):
+    self.gridSize = 2**pow2
+    
     self.windowHeight = wHeight
     self.windowWidth = wWidth
-    self.xpad = wWidth / 40
-    self.ypad = wHeight / 40
+    self.xpad = (wWidth - self.gridSize*bWidth)/2
+    self.ypad = (wHeight - self.gridSize*bWidth)/2
     self.boxWidth = bWidth
-
-    # Fill window with cells if nX and nY are not specified
-    if nX == 0 or nY == 0:
-      self.gridSizeX = (self.windowWidth - self.xpad)/self.boxWidth
-      self.gridSizeY = (self.windowHeight - self.ypad)/self.boxWidth
-    else:
-      self.gridSizeX = nX
-      self.gridSizeY = nY
     
     # Simulation starts paused
     self.running = False
@@ -80,10 +74,10 @@ class Life():
       self.ypad = wHeight / 10
     
     # Stores references to cells drawn in the window
-    self.cells = [[None for _ in np.arange(self.gridSizeY)] for _ in np.arange(self.gridSizeX)]
+    self.cells = [[None for _ in np.arange(self.gridSize)] for _ in np.arange(self.gridSize)]
 
     # 2D Matrices holding the current state and the temporary next state
-    self.life = np.zeros((self.gridSizeX, self.gridSizeY), dtype=bool)
+    self.life = np.zeros((self.gridSize, self.gridSize), dtype=bool)
     self.tlife = np.zeros_like(self.life)
     
     # 2D Matrix denoting cells that need to be redrawn
@@ -97,7 +91,7 @@ class Life():
     if self.uframe:
       self.draw_frame()
     self.draw_border()
-    self.draw_grid(1, self.gridSizeX-1, 1, self.gridSizeY-1)
+    self.draw_grid(1, self.gridSize-1, 1, self.gridSize-1)
 
     # OPTIONAL:  Add patterns to empty grid
 #    self.draw_pattern(blinkerPattern, 5, 5)
@@ -111,7 +105,9 @@ class Life():
     while True:
       try:
         if self.running:
+          ttime = time.time()
           self.update_grid()
+          print time.time() - ttime
         self.canvas.after(self.speed)
         self.canvas.update()
       except TclError:
@@ -126,10 +122,7 @@ class Life():
     liveNeighbors = np.sum(self.life[x-1:x+2,y-1:y+2])-self.life[x][y]
     
     if self.life[x][y]:
-      if liveNeighbors < 2:
-        self.tlife[x][y] = False
-        self.ulife[x][y] = True
-      elif liveNeighbors > 3:
+      if liveNeighbors < 2 or liveNeighbors > 3:
         self.tlife[x][y] = False
         self.ulife[x][y] = True
     else:
@@ -143,13 +136,13 @@ class Life():
     # whiteBack - Only dead cells in row behind current position
     # whiteCur - Only dead cells in current row
     # whiteFront - Only dead cells in front of current position
-    for i in np.arange(1, self.gridSizeX-1, 1):
+    for i in np.arange(1, self.gridSize-1, 1):
       whiteBack = True
       whiteCur = False
       if not self.life[i-1:i+2,1].any():
         whiteCur = True
 
-      for j in np.arange(1, self.gridSizeY-1, 1):
+      for j in np.arange(1, self.gridSize-1, 1):
         if not self.life[i-1:i+2,j+1].any():
           whiteFront = True
         else:
@@ -202,7 +195,7 @@ class Life():
     xmax = x + pat.shape[0]
     ymax = y + pat.shape[1]
 
-    if x < 1 or y < 1 or xmax > self.gridSizeX-1 or ymax > self.gridSizeY-1:
+    if x < 1 or y < 1 or xmax > self.gridSize-1 or ymax > self.gridSize-1:
       print('Pattern out of bounds')
       return
 
@@ -214,12 +207,12 @@ class Life():
 
   # Draw the bordering cells, which are always dead.
   def draw_border ( self ):
-    for i in np.arange(self.gridSizeX):
+    for i in np.arange(self.gridSize):
       self.draw_cell(i, 0)
-      self.draw_cell(i, self.gridSizeY-1)
-    for i in np.arange(1, self.gridSizeY-1, 1):
+      self.draw_cell(i, self.gridSize-1)
+    for i in np.arange(1, self.gridSize-1, 1):
       self.draw_cell(0, i)
-      self.draw_cell(self.gridSizeX-1, i)
+      self.draw_cell(self.gridSize-1, i)
 
   # Draw a red border around the board and display text at the top
   def draw_frame ( self ):
@@ -228,10 +221,10 @@ class Life():
     self.canvas.delete(self.titleText)
     self.canvas.delete(self.speedText)
     
-    twid = self.xpad+self.boxWidth*self.gridSizeX+1
+    twid = self.xpad+self.boxWidth*self.gridSize+1
     yo10 = self.ypad/10.
     xm1 = self.xpad-1
-    self.topBorder = self.canvas.create_rectangle(xm1, self.ypad-1, twid, self.ypad+self.boxWidth*self.gridSizeY+1, outline='red')
+    self.topBorder = self.canvas.create_rectangle(xm1, self.ypad-1, twid, self.ypad+self.boxWidth*self.gridSize+1, outline='red')
 
     self.tlifeBorder = self.canvas.create_rectangle(xm1, yo10, self.windowWidth-self.xpad+1, self.ypad-yo10, outline='red', fill='white')
     self.titleText = self.canvas.create_text(self.windowWidth/3., 5.*yo10, text='Conway\'s Game of Life', font=self.font1)
@@ -267,7 +260,7 @@ class Life():
   def click ( self, event ):
     x = int(np.floor((event.x-self.xpad)/self.boxWidth))
     y = int(np.floor((event.y-self.ypad)/self.boxWidth))
-    if x < self.gridSizeX-1 and y < self.gridSizeY-1 and x > 0 and y > 0:
+    if x < self.gridSize-1 and y < self.gridSize-1 and x > 0 and y > 0:
       self.tlife[x][y] = self.life[x][y] = not self.life[x][y]
       self.ulife[x][y] = True
       self.draw_cell(x, y)
@@ -278,19 +271,19 @@ class Life():
     self.tlife = np.zeros_like(self.tlife)
     self.life[:,:] = self.tlife[:,:]
     self.ulife = np.ones_like(self.tlife)
-    self.draw_grid(0, self.gridSizeX, 0, self.gridSizeY)
+    self.draw_grid(0, self.gridSize, 0, self.gridSize)
 
   # 'r' - Create life randomly on the board. 50/50 : dead/alive
   def random_life ( self, event ):
-    for i in np.arange(1, self.gridSizeX-1, 1):
-      for j in np.arange(1, self.gridSizeY-1, 1):
+    for i in np.arange(1, self.gridSize-1, 1):
+      for j in np.arange(1, self.gridSize-1, 1):
         if np.round(np.random.rand()) == 1:
           self.tlife[i][j] = True
         else:
           self.tlife[i][j] = False
     self.life[:,:] = self.tlife[:,:]
     self.ulife = np.ones_like(self.tlife)
-    self.draw_grid(0, self.gridSizeX, 0, self.gridSizeY)
+    self.draw_grid(0, self.gridSize, 0, self.gridSize)
     
   # - Up Arrow - Speed up the simulation
   def speed_up ( self, event ):
@@ -320,4 +313,4 @@ class Life():
     self.quit()
  
 # Instatiate game
-game = Life(wWidth=800, wHeight=900)#, bWidth=24, nX=32, nY=32)
+game = Life(wWidth=800, wHeight=800, bWidth=12, pow2=6)
